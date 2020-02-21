5f0bab5fb3ca2f1fe4c000cadb81d0ae92fbba63
Herminio <herminio.eng@gmail.com>
2020-02-21 19:39:22 +0000

    Added optimization.py

--- Libs/Optimization.py
+++ Libs/Optimization.py
@@ -1,4 +1,26 @@
 from UtilitiesLib import computeMedia
+import numpy as np
+
+class CellOptimization(object):
+	def __init__(self):
+		self.cells = []
+
+	def addOptimizer(self, optimizer):
+		self.cells.append(optimizer)
+
+	def initialize(self):
+		self.size = len(self.cells)
+		self.deltas = np.ones(self.size)
+		for i in range(self.size):
+			self.deltas[i] = self.cells[i].delta
+
+	def optimize(self, ratesOnCells):
+		for i in range(self.size):
+			self.deltas[i] = self.cells[i].computeDelta(ratesOnCells[i])
+		return self.deltas
+
+
+
 
 class BackForwardStep(object):
 	def __init__(self, delta_initial, step, factor=10., restart=True):
@@ -15,6 +37,53 @@ class BackForwardStep(object):
 		if len(self.rates) < 2:
 			return self.delta
 		else:
+			# if abs(self.rates[-1] - self.rates[-2]) < 1e-2:
+			# 	self.step = self.initialStep
+			if self.rates[-1] > self.rates[-2]:
+				self.__increase()
+			else:
+				self.__reverse()
+				self.__increase()
+		return self.delta
+
+	def __increase(self):
+		self.delta += self.step
+		self.__saveDelta()
+
+	def __reverse(self):
+		self.step = -self.step
+		self.__reduceStep()
+
+	def __reduceStep(self):
+		self.step = self.step/self.factor
+
+	def __saveDelta(self):
+		self.deltas.append(self.delta)
+
+	def __saveRate(self, rate):
+		if len(self.rates) > 2:
+			self.rates.pop(0)
+		self.rates.append(rate)
+
+
+
+class BackTracking(object):
+	def __init__(self, delta_initial, step, factor=10., restart=True):
+		self.delta = delta_initial
+		self.deltas = [self.delta]
+		self.rates = []
+		self.initialStep = step
+		self.step = self.initialStep
+		self.factor = factor
+		self.restart = restart
+
+	def computeDelta(self, rate):
+		self.__saveRate(rate)
+		if len(self.rates) < 2:
+			return self.delta
+		else:
+			if abs(self.rates[-1] - self.rates[-2]) < 1e-2:
+				self.step = self.initialStep
 			if self.rates[-1] > self.rates[-2]:
 				self.__increase()
 			else:
